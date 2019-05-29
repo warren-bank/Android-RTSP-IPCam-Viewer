4be3d573de1652b3fe699eddc85001a5707a8aa8
Warren R. Bank <warren.r.bank@gmail.com>
2019-05-28 23:22:14 -0700

    failed attempt: replace `VideoView` with ExoPlayer `PlayerView`

--- RTSP-IPCam-Viewer/src/main/java/com/github/warren_bank/rtsp_ipcam_viewer/list_view/recycler_view/RecyclerViewHolder.java
+++ RTSP-IPCam-Viewer/src/main/java/com/github/warren_bank/rtsp_ipcam_viewer/list_view/recycler_view/RecyclerViewHolder.java
@@ -1,29 +1,48 @@
 package com.github.warren_bank.rtsp_ipcam_viewer.list_view.recycler_view;
 
+import com.github.warren_bank.rtsp_ipcam_viewer.R;
 import com.github.warren_bank.rtsp_ipcam_viewer.common.data.VideoType;
 import com.github.warren_bank.rtsp_ipcam_viewer.fullscreen_view.activities.VideoActivity;
 
 import androidx.recyclerview.widget.RecyclerView;
+import android.content.Context;
 import android.net.Uri;
 import android.view.View;
 
-import com.sprylab.android.widget.TextureVideoView;
+import com.google.android.exoplayer2.DefaultRenderersFactory;
+import com.google.android.exoplayer2.ExoPlayerFactory;
+import com.google.android.exoplayer2.RenderersFactory;
+import com.google.android.exoplayer2.SimpleExoPlayer;
+import com.google.android.exoplayer2.source.ExtractorMediaSource;
+import com.google.android.exoplayer2.source.MediaSource;
+import com.google.android.exoplayer2.trackselection.DefaultTrackSelector;
+import com.google.android.exoplayer2.ui.PlayerView;
+import com.google.android.exoplayer2.upstream.DefaultHttpDataSourceFactory;
 
 public final class RecyclerViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener, View.OnLongClickListener {
 
-    private TextureVideoView view;
-    private boolean isPaused;
+    private PlayerView view;
+    private SimpleExoPlayer exoPlayer;
+    private DefaultHttpDataSourceFactory dataSourceFactory;
 
     private VideoType data;
 
     public RecyclerViewHolder(View view) {
         super(view);
 
-        this.view = (TextureVideoView) view;
-        this.isPaused = false;
+        this.view = (PlayerView) view;
+
+        Context context = view.getContext();
+        DefaultTrackSelector trackSelector = new DefaultTrackSelector();
+        RenderersFactory renderersFactory = new DefaultRenderersFactory(context);
+        this.exoPlayer = ExoPlayerFactory.newSimpleInstance(context, renderersFactory, trackSelector);
+
+        String userAgent = context.getResources().getString(R.string.user_agent);
+        this.dataSourceFactory = new DefaultHttpDataSourceFactory(userAgent);
 
         this.view.setOnClickListener(this);
         this.view.setOnLongClickListener(this);
+        this.view.setPlayer(this.exoPlayer);
     }
 
     public void bind(VideoType data) {
@@ -31,51 +50,39 @@ public final class RecyclerViewHolder extends RecyclerView.ViewHolder implements
 
         stop();
 
-        view.setVideoURI(
-            Uri.parse(data.URL_low_res)
-        );
+        Uri uri = Uri.parse(data.URL_low_res);
+        MediaSource source = new ExtractorMediaSource.Factory(dataSourceFactory).createMediaSource(uri);
+        exoPlayer.prepare(source);
 
         play();
     }
 
     public void play() {
         try {
-            if (isPaused)
-                view.resume();
-            else
-                view.start();
+            exoPlayer.setPlayWhenReady(true);
         }
         catch (Exception e){}
-
-        isPaused = false;
     }
 
     public void pause() {
         try {
-            view.pause();
+            exoPlayer.setPlayWhenReady(false);
         }
         catch (Exception e){}
-
-        isPaused = true;
     }
 
     public void stop() {
         try {
-            view.stopPlayback();
+            exoPlayer.stop(true);
         }
         catch (Exception e){}
-
-        isPaused = false;
     }
 
     public void release() {
         try {
-            stop();
-            view.suspend();
+            exoPlayer.release();
         }
         catch (Exception e){}
-
-        isPaused = false;
     }
 
     @Override
@@ -88,10 +95,12 @@ public final class RecyclerViewHolder extends RecyclerView.ViewHolder implements
 
     @Override
     public boolean onLongClick(View v) {
-        if (view.isPlaying())
-            pause();
-        else
-            play();
+        try {
+            exoPlayer.setPlayWhenReady(
+                !exoPlayer.getPlayWhenReady()
+            );
+        }
+        catch (Exception e){}
 
         return true;
     }
