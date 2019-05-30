f43176e2852b37e910f882436780f0ab67deaa5c
Warren R. Bank <warren.r.bank@gmail.com>
2019-05-30 02:12:05 -0700

    migrate "fullscreen" Activity: `VideoView` to ExoPlayer `PlayerView`

--- RTSP-IPCam-Viewer/src/main/java/com/github/warren_bank/rtsp_ipcam_viewer/fullscreen_view/activities/VideoActivity.java
+++ RTSP-IPCam-Viewer/src/main/java/com/github/warren_bank/rtsp_ipcam_viewer/fullscreen_view/activities/VideoActivity.java
@@ -7,12 +7,29 @@ import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
-import android.widget.VideoView;
+
+import com.google.android.exoplayer2.DefaultRenderersFactory;
+import com.google.android.exoplayer2.ExoPlayerFactory;
+import com.google.android.exoplayer2.RenderersFactory;
+import com.google.android.exoplayer2.SimpleExoPlayer;
+import com.google.android.exoplayer2.source.ExtractorMediaSource;
+import com.google.android.exoplayer2.source.MediaSource;
+import com.google.android.exoplayer2.source.rtsp.RtspDefaultClient;
+import com.google.android.exoplayer2.source.rtsp.RtspMediaSource;
+import com.google.android.exoplayer2.source.rtsp.core.Client;
+import com.google.android.exoplayer2.trackselection.DefaultTrackSelector;
+import com.google.android.exoplayer2.ui.AspectRatioFrameLayout;
+import com.google.android.exoplayer2.ui.PlayerView;
+import com.google.android.exoplayer2.upstream.DefaultHttpDataSourceFactory;
+import com.google.android.exoplayer2.util.Util;
 
 public class VideoActivity extends AppCompatActivity {
     private static final String EXTRA_URL = "URL";
 
-    private VideoView video;
+    private PlayerView view;
+    private SimpleExoPlayer exoPlayer;
+    private DefaultHttpDataSourceFactory dataSourceFactory;
+    private String url;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,18 +37,91 @@ public class VideoActivity extends AppCompatActivity {
 
         setContentView(R.layout.fullscreen_view_activities_videoactivity);
 
-        this.video = (VideoView) findViewById(R.id.video_view);
+        this.view = (PlayerView) findViewById(R.id.player_view);
+
+        Context context = (Context) this;
+        DefaultTrackSelector trackSelector = new DefaultTrackSelector();
+        RenderersFactory renderersFactory = new DefaultRenderersFactory(context);
+        this.exoPlayer = ExoPlayerFactory.newSimpleInstance(context, renderersFactory, trackSelector);
+
+        String userAgent = context.getResources().getString(R.string.user_agent);
+        this.dataSourceFactory = new DefaultHttpDataSourceFactory(userAgent);
+
+        this.view.setUseController(true);
+        this.view.setResizeMode(AspectRatioFrameLayout.RESIZE_MODE_FIXED_WIDTH);
+        this.view.setPlayer(this.exoPlayer);
 
         Intent intent = getIntent();
-        String url;
-
         if (intent.hasExtra(EXTRA_URL)) {
-            url = intent.getStringExtra(EXTRA_URL);
+            this.url = intent.getStringExtra(EXTRA_URL);
 
-            video.setVideoURI(
-                Uri.parse(url)
-            );
+            prepare();
         }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        play();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        stop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        release();
+    }
+
+    private void prepare() {
+        Uri uri = Uri.parse(this.url);
+        MediaSource source;
+
+        if (Util.isRtspUri(uri)) {
+            source = new RtspMediaSource.Factory(RtspDefaultClient.factory()
+                .setFlags(Client.FLAG_ENABLE_RTCP_SUPPORT)
+                .setNatMethod(Client.RTSP_NAT_DUMMY))
+                .createMediaSource(uri);
+        } else {
+            source = new ExtractorMediaSource.Factory(dataSourceFactory).createMediaSource(uri);
+        }
+
+        exoPlayer.prepare(source);
+    }
+
+    private void play() {
+        try {
+            exoPlayer.setPlayWhenReady(true);
+        }
+        catch (Exception e){}
+    }
+
+    private void pause() {
+        try {
+            exoPlayer.setPlayWhenReady(false);
+        }
+        catch (Exception e){}
+    }
+
+    private void stop() {
+        try {
+            exoPlayer.stop(true);
+        }
+        catch (Exception e){}
+    }
+
+    private void release() {
+        try {
+            exoPlayer.release();
+        }
+        catch (Exception e){}
     }
 
     public static void open(Context context, String url) {
